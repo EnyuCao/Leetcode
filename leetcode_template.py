ffe7014b0016eb2a8b5ee51e74e3bcf2cc418cab
EnyuCao <caoe@kth.se>
2018-10-08 16:28:19 +0200

    Update leetcode_template.py

--- leetcode_template.py
+++ leetcode_template.py
@@ -1,33 +1,43 @@
 # Leetcode Template
-class Solution(object):
-    def twoSum(self, nums, target):
+
+class Solution(object):
+    def functionName(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
+        :type input1: List[int]
+        :type input2: int
         :rtype: List[int]
         """
-#
-#  Content: use return 
-#
+# Write code here!
+
+#  Content: use return 
+
+# Finish coding here
         return "ERROR"
         
 
-
-
-# Local Test Template
+
+
+
+
+
+# Local Test Template
 
 while True:
     try:
         #input_str = raw_input()
-# Manually input
-#
-# Input:
-#
-   	num =  [2, 7, 11, 15]
-    	target = 9
-# Functionality
-#
-# Content£º Use print
-#
+
+        # Manually input
+        #
+        # Input:
+        #
+
+        num =  [2, 7, 11, 15]
+        target = 9
+        # Functionality
+        #
+        # Use print
+        #
     except:
         break   
+    else:
+        raw_input("Next input(s)")
