f8add464290755e1522188c1ba7352d5efb9ebd1
Enyu Cao <caoe@kth.se>
2018-10-08 16:18:06 +0200

    Restart this repo again!

--- addTwoNumbers.py
+++
@@ -1,13 +0,0 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-idx = ListNode(3)
-n = idx
-n.next = ListNode(4)
-n = n.next
-n.next = ListNode(5)
-n = n.next
-print idx.val, idx.next.val
